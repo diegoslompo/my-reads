8fe4d1dd099f8a3f7ac9523d8a5081db8c3d523b
Diego <diegoslompo@gmail.com>
2018-07-31 23:45:51 -0300

    Feat - New methods to application separete per view

--- src/App.js
+++ src/App.js
@@ -17,20 +17,43 @@ class BooksApp extends React.Component {
     })
   }
 
+  
+  
   render() {
+
+    let currentlyReadingBooks =  this.state.books.filter(book => {
+      if(book.shelf === "currentlyReading")
+        return book
+    })
+    let wantToReadBooks =  this.state.books.filter(book => {
+      if(book.shelf === "wantToRead")
+        return book
+    })
+    let readBooks =  this.state.books.filter(book => {
+      if(book.shelf === "read")
+        return book
+    })
+
     return (
       <div className="app">
 
         <SearchBooks 
-          className="search-books"
+          className="search-books"    
+        />
+
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
           
-        />
+          <BookShelf className="list-books" books={currentlyReadingBooks} titleSection="Currently Read"/>
+          <BookShelf className="list-books" books={wantToReadBooks} titleSection="Want To Read"/>
+          <BookShelf className="list-books" books={readBooks} titleSection="Read"/>
 
-        <BookShelf
-          className="list-books"
-          books={this.state.books}
-        />
-
+          <div className="open-search">
+            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          </div>
+        </div>
       </div>
     )
   }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,52 +1,47 @@
 import React, {Component} from 'react'
-
+//import BookList from './BookList'
 
 class BookShelf extends Component {
+  
+  state = {
+    section: ''
+  }
 
   render() {
 
-    const { showBooks } = this.props
-
     return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
         <div className="list-books-content">
             <div>
               <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.props.books.map((book) => (
-                    <li>
-                      <div className="book">
+                <h2 className="bookshelf-title">{this.props.titleSection}</h2>          
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {this.props.books.map((book) => (
+                    <li key={book.id}>
+                        <div className="book">
                         <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
                             <select>
-                              <option value="move" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
                             </select>
-                          </div>
+                            </div>
                         </div>
                         <div className="book-title">{book.title}</div>
                         <div className="book-authors">{book.author}</div>
-                      </div>
-                    </li>
-                  ))}
-                </ol>
+                        </div>
+                      </li>
+                    ))}
+                  </ol> 
+                  {/*<BookList shelf={this.props.books} />*/}       
+                </div> 
               </div>
             </div>
-          </div>
         </div>
-        <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-        </div>
-      </div>
     )
   }
 }
