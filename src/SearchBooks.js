df572f59b3b52c5b0d78d43b2bad59f48fff0c44
diegoslompo <diegoslompo@gmail.com>
2018-08-23 00:18:21 -0300

    fix update query corretly

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
+import NotificationSearch from './NotificationSearch'
 import * as BooksAPI from './BooksAPI'
 
  
@@ -21,15 +22,12 @@ class SearchBooks extends Component {
   newBook = (query) => {
     if (query) {
       BooksAPI.search(query).then((result) => {
-        if(result.length > 0) {
-          console.log("entrou")
+        if(this.state.query !== '' && result.length > 0) {
           this.setState({searchBooks: result, searchPage: true })
          } else {
-          console.log("saiu")
           this.setState({searchBooks: '', searchPage: false})
         }
       })
-
     } else {
       this.setState({searchBooks: '', searchPage: false})
     }
@@ -143,23 +141,16 @@ class SearchBooks extends Component {
         </div>
         <div className="ds-search__list">
           
+          {(query !== '' && searchPage === false) && (
+            <ComponentWithSkeletonState {...fakeGraphQLResult} />
+          )}
           {( query !== '' &&  searchPage === true) && (
             <Book shelf={searchBooks} onUpdate={onUpdateSection} />
           )}
-          {(query !== '' && searchPage == false) && (
-            <ComponentWithSkeletonState {...fakeGraphQLResult} />
-          )}
           {(query === '' && searchPage === false) && (
-            <div className="ds-error__not-found">  
-                <p className="ds-error--no-happy">;)</p>      
-                <p className="ds-error__info">
-                  <b>Hello... Search by book or author</b><br/>
-                  do your search again or use the terms:
-                </p>
-                <p className="ds-error__terms">'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'</p>
-            </div>
+            <NotificationSearch/>
           )}
-          {/* bug do result para contornar */}
+          {/* bug do result para contornar
           {(query === '' && searchPage === true) && (
             <div className="ds-error__not-found">  
                 <p className="ds-error--no-happy">;)</p>      
@@ -169,7 +160,7 @@ class SearchBooks extends Component {
                 </p>
                 <p className="ds-error__terms">'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'</p>
             </div>
-          )}
+          )} */}
  
         </div>
       </div>
